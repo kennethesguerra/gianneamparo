7c88f38b2ee58abb348c5af3341cb70292bafb13
Mark Kenneth G. Esguerra <esguerrakenneth@gmail.com>
2020-08-21 14:08:46 +0800

    Updates

---
+++ precache-manifest.8816b3061d3c190bc1c79044bc711ad5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e84a35d1278e3e3fbde07435657200",
+    "url": "/gianneamparo/index.html"
+  },
+  {
+    "revision": "24c73c6c6efc1e47e073",
+    "url": "/gianneamparo/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3428f7318fd58b68c3e9",
+    "url": "/gianneamparo/static/css/main.4f3b5ded.chunk.css"
+  },
+  {
+    "revision": "24c73c6c6efc1e47e073",
+    "url": "/gianneamparo/static/js/2.3cb72fbf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/gianneamparo/static/js/2.3cb72fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3428f7318fd58b68c3e9",
+    "url": "/gianneamparo/static/js/main.c3d79ad6.chunk.js"
+  },
+  {
+    "revision": "620b18fbb41b29c7533e",
+    "url": "/gianneamparo/static/js/runtime-main.c2dd28b8.js"
+  },
+  {
+    "revision": "5ce0ec49151d816a952d4d22a7eaf550",
+    "url": "/gianneamparo/static/media/289-200x300.5ce0ec49.jpg"
+  },
+  {
+    "revision": "6362cb17f2a38f27feea0b4947d1c59a",
+    "url": "/gianneamparo/static/media/gianne.6362cb17.jpg"
+  },
+  {
+    "revision": "252c8cadcb2b4e269de7f6e3a21ad03f",
+    "url": "/gianneamparo/static/media/logo.252c8cad.jpg"
+  },
+  {
+    "revision": "87e7a6b57940cdba261a1f78c645bc76",
+    "url": "/gianneamparo/static/media/logo.87e7a6b5.png"
+  }
+]);
